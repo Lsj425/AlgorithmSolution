436dc68404761cbd5dc43fe4e698165b7a8d0076
三金 <42901355+Lsj425@users.noreply.github.com>
2019-09-09 20:15:11 +0800

    Create solution2.java

---
+++ 01-二维数组查找/solution2.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public boolean Find(int target, int [][] array) {
+        for(int i = 0;i < array.length; i++){
+            int low = 0;
+            int high = array[i].length - 1;
+            while(low <= high){
+                int mid = (low + high)/2;
+                if(target > array[i][mid]){
+                    low = mid+1;
+                }else if(target<array[i][mid]){
+                    high = mid-1;
+                }else{
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
