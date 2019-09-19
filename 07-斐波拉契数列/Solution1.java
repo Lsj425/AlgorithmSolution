19156d43c5433e0c49399967e943c9fb4cc7b8be
三金 <42901355+Lsj425@users.noreply.github.com>
2019-09-19 12:09:33 +0800

    Create Solution1.java

---
+++ 07-斐波拉契数列/Solution1.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int Fibonacci(int n) {
+        if(n <= 1){
+            return n;
+        }
+        int f1 = 1, f2 = 2, next = 0;
+        for(int i = 3; i <= n; i++){
+            next = f1 + f2;
+            f1 = f2;
+            f2 = next;
+        }
+        return next;
+    }
+}
