850d09383847aa9d860f4bff311a2b7bf098a5f9
三金 <42901355+Lsj425@users.noreply.github.com>
2019-09-29 09:39:19 +0800

    Update Solution.java

--- 22-从上往下打印二叉树/Solution.java
+++ 22-从上往下打印二叉树/Solution.java
@@ -1,4 +1,38 @@
-## 题目描述
-从上往下打印出二叉树的每个节点，同层节点从左至右打印。
-## 解题思路
-每一次打印一个节点的时候，如果该节点有子节点，则把该节点的子节点放到一个队列的尾部。接下来到对队列的头部取出最早进入队列的节点放到ArrayList 中，重复前面的操作，直至队列中所有的节点都存到ArrayList中。  
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
+
+/**
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+*/
+public class Solution {
+    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+        ArrayList<Integer> list = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if(root == null){
+            return list;
+        }
+        queue.offer(root); // 节点入队
+        while(!queue.isEmpty()){
+            TreeNode head = queue.poll(); // 节点出队
+            list.add(head.val); // 将当前节点值存入结果队列
+            if(head.left != null){
+                queue.offer(head.left); // 左节点入队
+            }
+            if(head.right != null){
+                queue.offer(head.right); // 右节点入队
+            }
+        }
+        return list;
+    }
+}
