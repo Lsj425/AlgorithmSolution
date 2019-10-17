6b842da6d6dc0b577c29760258ba84105980d9f0
三金 <42901355+Lsj425@users.noreply.github.com>
2019-10-17 08:22:03 +0800

    Create Solution2.java

---
+++ 58-对称的二叉树/Solution2.java
@@ -0,0 +1,33 @@
+/*
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
+*/
+import java.util.Stack;
+public class Solution {
+    boolean isSymmetrical(TreeNode pRoot){
+        if(pRoot == null) return true;
+        Stack<TreeNode> s = new Stack<>();
+        s.push(pRoot.left);
+        s.push(pRoot.right);
+        while(!s.empty()) {
+            TreeNode right = s.pop();// 成对取出
+            TreeNode left = s.pop();
+            if(left == null && right == null) continue;
+            if(left == null || right == null) return false;
+            if(left.val != right.val) return false;
+            // 成对插入
+            s.push(left.left);
+            s.push(right.right);
+            s.push(left.right);
+            s.push(right.left);
+        }
+        return true;
+    }
+}
